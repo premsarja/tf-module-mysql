23bc9a389849246a26140b4f302beb3a9fcd7e39
premsarja <premsagar@prodot.in>
2024-03-14 18:20:48 +0530

    done

---
+++ cluster.tf
@@ -0,0 +1,29 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 10
+  db_name              = "roboshop-${var.ENV}-mysql"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "prem"
+  password             = "premsagar"
+  parameter_group_name = aws_db_parameter_group.default.name
+  skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.mysql.name
+}
+
+
+resource "aws_db_subnet_group" "mysql" {
+  name       = "roboshop-${var.ENV}-mysql-subnet-group"
+  subnet_ids = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS]
+
+  tags = {
+    Name = "roboshop-${var.ENV}-mysql-subnet-group"
+  }
+}
+
+
+resource "aws_db_parameter_group" "default" {
+  name   = "roboshop-${var.ENV}-mysql-pg"
+  family = "mysql5.6"
+
+  }
---
+++ sg.tf
@@ -0,0 +1,26 @@
+
+resource "aws_security_group" "allows_mysql" {
+  name        = "mysql allows internal traffic"
+  description = "Allow private traffics"
+  vpc_id =   data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description = "redis  from private network "
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR[1]]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-mysql-sg   "
+  }
+}
+
