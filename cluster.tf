b7c32800be9a5bb5449ba57f662a0f316d808030
premsarja <premsagar@prodot.in>
2024-03-14 18:30:35 +0530

    done

--- cluster.tf
+++ cluster.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "mysql" {
   allocated_storage    = 10
-  db_name              = "roboshop-${var.ENV}-mysql"
+  db_name              = "roboshop.${var.ENV}.mysql"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
