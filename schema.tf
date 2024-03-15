7e7f9de359f14d127b7da0f6406984aed3687b71
premsarja <premsagar@prodot.in>
2024-03-15 12:33:05 +0530

    done

---
+++ schema.tf
@@ -0,0 +1,15 @@
+
+resource "null_resource" "schema" {
+
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /tmp
+
+curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${aws_db_instance.address.mysql} -uprem -ppremsagar < shipping.sql
+EOF
+  }
+}
