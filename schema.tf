a961977673041b5ed6b1ac9704b281fa6617aad8
premsarja <premsagar@prodot.in>
2024-03-15 14:54:37 +0530

    done

--- schema.tf
+++ schema.tf
@@ -1,13 +1,13 @@
-
 resource "null_resource" "schema" {
-  depends_on = [aws_db_instance.RDS]  
+  depends_on = [aws_db_instance.RDS]
+
   provisioner "local-exec" {
-          command = <<EOF
-            cd /tmp      
-            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-            unzip -o /tmp/mysql.zip
-            cd mysql-main
-            mysql -h ${aws_db_instance.RDS.address} -uprem -ppremsagar < shipping.sql
-       EOF
-    }
+    command = <<EOF
+      cd /tmp      
+      curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+      unzip -o /tmp/mysql.zip
+      cd mysql-main
+      mysql -h ${aws_db_instance.RDS.address} -uprem -ppremsagar < shipping.sql
+    EOF
+  }
 }
