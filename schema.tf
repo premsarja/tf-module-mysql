1beae0bb1e90cdfc9310c3a6094fa84da020a3bc
premsarja <premsagar@prodot.in>
2024-03-15 19:26:43 +0530

    done

--- schema.tf
+++ schema.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "schema" {
 
-  provisioner "local-exec" {
+provisioner "local-exec" {
     command = <<EOF
       cd /tmp      
       curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
