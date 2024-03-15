5906d85defd41db5f48068b61484c0e3dee8f0d6
premsarja <premsagar@prodot.in>
2024-03-15 21:56:52 +0530

    done

--- schema.tf
+++ schema.tf
@@ -1,12 +1,14 @@
+# Injecting the schema
 resource "null_resource" "schema" {
-
-provisioner "local-exec" {
+  depends_on = [aws_db_instance.mysql]
+  
+  provisioner "local-exec" {
     command = <<EOF
-      cd /tmp      
+      cd /tmp 
       curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-      unzip -o /tmp/mysql.zip
+      unzip -o /tmp/mysql.zip 
       cd mysql-main
       mysql -h ${aws_db_instance.mysql.address} -uprem -ppremsagar < shipping.sql
-    EOF
+EOF
   }
 }
