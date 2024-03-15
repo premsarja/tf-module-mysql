b6ba231fbf7aaae757a92a3adce3f739aebc4136
premsarja <premsagar@prodot.in>
2024-03-15 12:40:03 +0530

    done

--- schema.tf
+++ schema.tf
@@ -9,7 +9,7 @@ cd /tmp
 curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
 unzip  mysql.zip
 cd mysql-main
-mysql -h ${aws_db_instance.address.RDS} -uprem -ppremsagar < shipping.sql
+mysql -h ${aws_db_instance.RDS.address} -uprem -ppremsagar < shipping.sql
 EOF
   }
 }
