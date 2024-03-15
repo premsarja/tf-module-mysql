19c47a70df350e56f8ffa740cd2f2bfbf173ba47
premsarja <premsagar@prodot.in>
2024-03-15 12:37:11 +0530

    done

--- schema.tf
+++ schema.tf
@@ -7,7 +7,7 @@ resource "null_resource" "schema" {
 cd /tmp
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-unzip -o mysql.zip
+unzip  mysql.zip
 cd mysql-main
 mysql -h ${aws_db_instance.address.mysql} -uprem -ppremsagar < shipping.sql
 EOF
