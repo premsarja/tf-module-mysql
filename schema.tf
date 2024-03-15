9c6efb18234019b4d3bbafa43ce36f9c2153f6ee
premsarja <premsagar@prodot.in>
2024-03-15 12:56:40 +0530

    done

--- schema.tf
+++ schema.tf
@@ -1,14 +1,12 @@
 
 resource "null_resource" "schema" {
-
-
   provisioner "local-exec" {
-    command = <<EOF
-      cd /tmp      
-      curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-      unzip -o /tmp/mysql.zip
-      cd mysql-main
-      mysql -h ${aws_db_instance.RDS.address} -uprem -ppremsagar < shipping.sql
-    EOF
-  }
+          command = <<EOF
+            cd /tmp      
+            curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+            unzip -o /tmp/mysql.zip
+            cd mysql-main
+            mysql -h ${aws_db_instance.RDS.address} -uprem -ppremsagar < shipping.sql
+       EOF
+    }
 }
