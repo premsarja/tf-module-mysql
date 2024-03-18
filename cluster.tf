4043034d9e3654a9008572302ec5c3f123dfb134
premsarja <premsagar@prodot.in>
2024-03-18 22:17:21 +0530

    done

--- cluster.tf
+++ cluster.tf
@@ -9,7 +9,7 @@ resource "aws_db_instance" "mysql" {
   password             = "premsagar"
   parameter_group_name = aws_db_parameter_group.default.name
   skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.mysql.name
+  db_subnet_group_name = aws_db_subnet_group.mysql.name_prefix
   vpc_security_group_ids = [aws_security_group.allow_mysql.id]
 }
 
--- schema.tf
+++ schema.tf
@@ -1,14 +1,12 @@
-# Injecting the schema
 resource "null_resource" "schema" {
-  depends_on = [aws_db_instance.mysql]
-  
-  provisioner "local-exec" {
+
+provisioner "local-exec" {
     command = <<EOF
-      cd /tmp 
+      cd /tmp      
       curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-      unzip -o /tmp/mysql.zip 
+      unzip -o /tmp/mysql.zip
       cd mysql-main
-      mysql -h roboshop-dev-mysql.cf6ic4gs8isj.us-east-1.rds.amazonaws.com -uprem -ppremsagar < shipping.sql
-EOF
+      mysql -h {$} -uprem -ppremsagar < shipping.sql
+    EOF
   }
 }
