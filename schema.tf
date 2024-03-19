d9b5b0774e439fa742f39c4b3107c33df8f08dad
premsarja <premsagar@prodot.in>
2024-03-19 10:50:15 +0530

    done

--- schema.tf
+++ schema.tf
@@ -1,12 +1,13 @@
 resource "null_resource" "schema" {
 
+depends_on = [aws_db_instance.mysql]  
 provisioner "local-exec" {
     command = <<EOF
       cd /tmp      
       curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
       unzip -o /tmp/mysql.zip
       cd mysql-main
-      mysql -h {$} -uprem -ppremsagar < shipping.sql
+      mysql -h ${aws_db_instance.mysql.address} -uprem -ppremsagar < shipping.sql
     EOF
   }
 }
