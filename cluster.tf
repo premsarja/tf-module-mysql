3c8506380b7d82924a92595d7a92d2bf159cee2f
premsarja <premsagar@prodot.in>
2024-03-19 15:08:48 +0530

    done

--- cluster.tf
+++ cluster.tf
@@ -16,7 +16,7 @@ resource "aws_db_instance" "mysql" {
 
 resource "aws_db_subnet_group" "mysql" {
   name       = "roboshop-${var.ENV}-mysql-subnet-group"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR
+  subnet_ids = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS]
   tags = {
     Name = "roboshop-${var.ENV}-mysql-subnet-group"
   }
